22e2a22c41d823ef5008ac0ca20119ff7f6cc303
Jaime Raczka <jaime.raczka@gmail.com>
2014-07-30 21:45:08 -0400

    Fixed Bike Inventory and Completed Actions

--- BicycleIndustry.py
+++ BicycleIndustry.py
@@ -10,7 +10,7 @@ class Wheel(object):
 
 class Frame(object):
 
-	def __init__(self,frame_material,frame_weight,frame_manuf_cost):
+	def __init__(self, frame_material, frame_weight, frame_manuf_cost):
 		self.frame_material = frame_material
 		self.frame_weight = frame_weight
 		self.frame_manuf_cost = frame_manuf_cost
@@ -19,7 +19,7 @@ class Frame(object):
 class BicycleModel(object):
 
 # A bike has two of the same wheel and a frame 
-	def __init__(self,bike_name,bike_manuf,bike_wheel,bike_frame):		
+	def __init__(self, bike_name, bike_manuf, bike_wheel, bike_frame):		
 		self.bike_name = bike_name
 		self.bike_manuf = bike_manuf
 		self.bike_wheel = bike_wheel
@@ -30,33 +30,25 @@ class BicycleModel(object):
 
 class Bicycle_Manuf(object):
 
-	manuf_price = bike_cost + manuf_profit
-
-	def __init__(self,manuf_name):
+	def __init__(self, manuf_name, manuf_profit)
 		self.manuf_name = manuf_name
-
-    def manuf_profit():
-        # TODO: Implement this
-        # This should return the profit for the manufacturer
-        pass
+		self.manuf_profit = manuf_profit
+		self.manuf_price = bike_cost + manuf_profit
 
 
 class Bike_Shop(object):
 
-	bike_shop_price = manuf_price + bike_shop_profit
-
-	def __init__(self,bike_shop_name, bike_shop_profit=0.20, bike_shop_inventory=[]):
+	def __init__(self, bike_shop_name, bike_shop_profit=0.20, bike_shop_inventory=[]):
 		self.bike_shop_name = bike_shop_name
 		self.bike_shop_profit = bike_shop_profit
 		self.bike_shop_inventory = bike_shop_inventory
+		self.bike_shop_price = manuf_price + bike_shop_profit
 
     def add_bike_to_inventory(self, some_bike):
         self.bike_shop_inventory.append(some_bike)
 
-	def bike_shop_inventory():
-        # TODO
-		## WHAT IS THE CODE THAT SAYS BIKES GO HERE? 
-        pass
+    def remove_bike_from_inventory(self, some_bike):
+    	self.bike_shop_inventory.remove(some_bike)
 
 
 class Customer(object):
--- SellingBikes.py
+++ SellingBikes.py
@@ -1,7 +1,6 @@
 from BicycleIndustry import *
 
 
-
 road_wheel = Wheel(2,5,"RoadWheel")
 BeachWheel = Wheel(3,8,"BeachWheel")
 TrackWheel = Wheel(4,10,"TrackWheel")
@@ -26,48 +25,45 @@ JoceBikeShop = Bike_Shop("JoceBikeShop")
 JoceBikeShop.add_bike_to_inventory(Bike1)
 JoceBikeShop.add_bike_to_inventory(Bike2)
 JoceBikeShop.add_bike_to_inventory(Bike3)
-# JoceBikeShop.bike_shop_inventory(Bike1,Bike2,Bike3,Bike4,Bike5,Bike6)
+JoceBikeShop.add_bike_to_inventory(Bike4)
+JoceBikeShop.add_bike_to_inventory(Bike5)
+JoceBikeShop.add_bike_to_inventory(Bike6)
+
 
 CustJudy = Customer("CustJudy",200)
 CustLiz = Customer("CustLiz", 500)
 CustAndrea = Customer("CustAndrea",1000)
 
 customer_list = []
+customer_list.append(CustJudy)
+customer_list.append(CustLiz)
+customer_list.append(CustAndrea)
 
 for some_bike in JoceBikeShop.bike_shop_inventory:
     print some_bike.bike_name, some_bike.bike_weight
 
-print Bike1.bike_name, Bike1.bike_weight
-print Bike2.bike_name, Bike2.bike_weight
-print Bike3.bike_name, Bike3.bike_weight
-print Bike4.bike_name, Bike4.bike_weight
-print Bike5.bike_name, Bike5.bike_weight
-print Bike6.bike_name, Bike6.bike_weight
 
+for Customer in customer_list:
+	print Customer.cust_name "can afford the following bikes: "
+	for some_bike in bike_shop_inventory:
+		where some_bike.bike_shop_price <= Customer.cust_funds
+		print some_bike.bike_name
+	
 
-## I should be able to make a "for" loop to execute these three things, but I'm not sure what they're "in"
-print CustJudy.cust_name " can afford the following bikes: "
-for bike in bike_shop_inventory:
-	where bike_shop_price <= CustJudy.cust_funds
-	print bike_name
+print JoceBikeShop.bike_shop_name "has the following bikes as opening inventory:", JoceBikeShop.bike_shop_inventory
 
-print CustLiz.cust_name " can afford the following bikes: "
-for bike in bike_shop_inventory:
-	where bike_shop_price <= CustLiz.cust_funds
-	print bike_name
+print Customer.cust_name(CustJudy) "purchased Bike1"
+JoceBikeShop.remove_bike_from_inventory(Bike1)
+print Customer.cust_funds(CustJudy) - JoceBikeShop.bike_shop_price(Bike1)
 
-print CustAndrea.cust_name " can afford the following bikes: "
-for bike in bike_shop_inventory:
-	where bike_shop_price <= CustAndrea.cust_funds
-	print bike_name
+print Customer.cust_name(CustLiz) "purchased Bike2"
+JoceBikeShop.remove_bike_from_inventory(Bike2)
+print Customer.cust_funds(CustLiz) - JoceBikeShop.bike_shop_price(Bike2)
 
-print JoceBikeShop.bike_shop_name "has the following bikes as opening inventory:", bike_shop_inventory
+print Customer.cust_name(CustAndrea) "purchased Bike3"
+JoceBikeShop.remove_bike_from_inventory(Bike3)
+print Customer.cust_funds(CustAndrea) - JoceBikeShop.bike_shop_price(Bike3)
 
-for Customer:
-	print cust_name "purchased %s for %s, and has %s leftover" % (bike_name,bike_shop_price,cust_funds-bike_shop_price)
+print JoceBikeShop.bike_shop_name "has the following bikes as opening inventory:", JoceBikeShop.bike_shop_inventory
 
-## PUT WHO BOUGHT WHAT BIKE WHERE HERE. 
-
-## PRINT OUT REMAINING INVENTORY
-
-## PRINT OUT PROFIT
+print JoceBikeShop.bike_shop_name "made a profit of: " (JoceBikeShop.bike_shop_price(Bike1) + JoceBikeShop.bike_shop_price(Bike2) + JoceBikeShop.bike_shop_price(Bike3)) * JoceBikeShop.bike_shop_profit
